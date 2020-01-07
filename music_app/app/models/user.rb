d377196e5ba0a3ccb271091609c021880cf308ab
Rob Zahn <rob.jzahn@gmail.com>
2020-01-07 15:25:20 -0500

    Create user validations

--- music_app/app/models/user.rb
+++ music_app/app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+
+    validates :email, :password_digest, :session_token, presence: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+    validates :email, :session_token, uniqueness: true
+
+    def self.generate_session_token
+
+    end
+    
+    def reset_session_token! 
+
+    end
+    
+    def ensure_session_token
+
+    end
 end
