4cb9a7866b2deae3fc240985720f23d25211a8a8
Rob Zahn <rob.jzahn@gmail.com>
2020-01-07 20:02:58 -0500

    Add user model

--- music_app/app/models/user.rb
+++ music_app/app/models/user.rb
@@ -4,15 +4,40 @@ class User < ApplicationRecord
     validates :password, length: {minimum: 6, allow_nil: true}
     validates :email, :session_token, uniqueness: true
 
-    def self.generate_session_token
+    after_initialize :ensure_session_token
 
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return nil if user.nil?
+        
+        if user.is_password?(password)
+            user
+        else
+            nil
+        end
     end
-    
-    def reset_session_token! 
 
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest)
+        .is_password?(password)
     end
-    
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.base64(64)
+        self.save!
+        self.session_token
+    end
+
+    private
+
     def ensure_session_token
-
+        self.session_token ||= SecureRandom.base64(64)
     end
+end
+
 end
